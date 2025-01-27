ef2ff8f0c88633955a15efce946bd764194b2212
Shubham Jha <shubham19299@gmail.com>
2025-01-27 12:02:33 +0530

    DataTypeasignment

--- src/com/assignments/DataTypeAssignment.java
+++ src/com/assignments/DataTypeAssignment.java
@@ -3,8 +3,46 @@ package com.assignments;
 public class DataTypeAssignment {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		float temp =25.5f;
+			System.out.println(temp );
+		boolean orderplaced =true;
+			System.out.println(orderplaced );
+		int phoneno =1234567890;
+			System.out.println(phoneno);
+		float bankamount =1000.50f;
+			System.out.println(bankamount);
+		String email="john.doe@gmail.com";
+			System.out.println(email);
+		 double latitude =33.7749;
+		 double longitude =-122.4194;
+		 	System.out.println(latitude);
+		 	System.out.println(longitude);
+		 boolean maritalstatus =false;
+		 	System.out.println(maritalstatus);
+		 String occupation ="Software engineer";
+		 	System.out.println(occupation);
+		 String favcolor="blue";
+		 	
+		 short curntyear =2025;
+		 
+		 int instafollowers =1000000;
+		 System.out.println(instafollowers);
+		 float movierating = 7.5f;
+		 System.out.println(movierating);
+		 char bloodtype ='A';
+		 
+		 String booktitle ="To kill a Mopckingbird";
+		 
+		 short Noofemp=500;
+		 
+		 String timeofevent ="2.30 PM";  
+		 
+		 float timeoffevent = 2.30f;
+		 String NameofCountry="United States";
+		 String eyecolor ="brown";
+		 String birthplace ="New York City";
+		 float distancebtwncity = 200.5f;
 	}
 
 }
